8cb60d1a81227478c449c1547edf4c61379207d8
Arnaud Zheng <arnaud-zg@users.noreply.github.com>
2020-01-16 15:34:17 +0100

    feat(tick): use requestAnimationFrame instead of setInterval (#10)
    * feat(tick): use requestAnimationFrame instead of setInterval
    
    * fix: call method closeExternalWindow

--- __tests__/should-stop-checking-on-close.js
+++ __tests__/should-stop-checking-on-close.js
@@ -4,30 +4,42 @@ import OauthPopup from "../src/index.js";
 
 jest.useFakeTimers()
 
-test('OauthPopup stops checking for code when popup closes', () => {
-    const mockCallBack = jest.fn();
-    global.open = jest.fn().mockReturnValue({
-        location: "http://www.google.com?code=amazing"
+describe('OauthPopup', () => {
+    beforeEach(() => {
+        jest.spyOn(window, 'requestAnimationFrame').mockImplementation(cb => cb);
     });
 
-    const component = renderer.create(
-        <OauthPopup
-            onCode={mockCallBack}
-        >
-            <div>coooooooool</div>
-            <h2>siiiiiiiick</h2>
-            <h2>leeegiiiiit</h2>
-            <button>Doooooope</button>
-        </OauthPopup>
-    );
-    let popup = component.toJSON();
-    expect(popup).toMatchSnapshot();
-    popup.props.onClick();
-    popup = component.toJSON();
-    expect(popup).toMatchSnapshot();
-    expect(global.open).toBeCalled();
-    jest.runAllTimers();
-    expect(setInterval).toHaveBeenCalledTimes(1);
-    expect(clearInterval).toHaveBeenCalledTimes(1);
-    expect(mockCallBack).toHaveBeenCalledTimes(1);
-});
+    afterEach(() => {
+        window.requestAnimationFrame.mockRestore();
+    });
+
+    test('OauthPopup stops checking for code when popup closes', () => {
+        const mockCallBack = jest.fn();
+        global.open = jest.fn().mockReturnValue({
+            location: "http://www.google.com?code=amazing"
+        });
+
+        const component = renderer.create(
+            <OauthPopup
+                onCode={mockCallBack}
+            >
+                <div>coooooooool</div>
+                <h2>siiiiiiiick</h2>
+                <h2>leeegiiiiit</h2>
+                <button>Doooooope</button>
+            </OauthPopup>
+        );
+        let popup = component.toJSON();
+        expect(popup).toMatchSnapshot();
+        popup.props.onClick();
+        popup = component.toJSON();
+        expect(popup).toMatchSnapshot();
+        expect(global.open).toBeCalled();
+        jest.runAllTimers();
+        setTimeout(() => {
+            expect(requestAnimationFrame).toHaveBeenCalledTimes(1);
+            expect(cancelAnimationFrame).toHaveBeenCalledTimes(1);
+            expect(mockCallBack).toHaveBeenCalledTimes(1);
+        }, 1)
+    });
+})
