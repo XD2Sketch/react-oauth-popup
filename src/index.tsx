f8cc46aadc5c62813c6b358acb47835bc6f6e152
Kostas Kapetanakis <kapekost@gmail.com>
2021-01-26 09:50:33 +0000

    fix: prevent infinite onClose call (#32)

--- src/index.tsx
+++ src/index.tsx
@@ -74,7 +74,7 @@ const OauthPopup: React.FC<IPopupProps> = ({
       if (externalWindow) externalWindow.close();
       if (onClose) onClose();
     };
-  });
+  }, [externalWindow]);
 
   return (
     // eslint-disable-next-line
